2c7f74274955355a2d8f2a78a287958129d4a7c1
Yuri Salgado Farias Torres <yuri.salgado.ft@gmail.com>
2023-07-11 20:11:52 -0300

    Exercício 4 feito

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 type Props = {
   handleForm: (prop: boolean) => void
@@ -6,25 +6,95 @@ type Props = {
 
 export default function Form(props: Props) {
   const { handleForm } = props;
+
+  const [formInfo, setFormInfo] = useState({
+    name: '',
+    login: '',
+    senha: '',
+    url: '',
+  });
+  const [registerButton, setRegisterButton] = useState(true);
+
+  function handleChange(
+    event: React.ChangeEvent<
+    HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement
+    >,
+  ) {
+    const { name, value } = event.target;
+    setFormInfo({
+      ...formInfo,
+      [name]: value,
+    });
+    validateForm();
+  }
+
+  function validateForm() {
+    const { name, login, senha } = formInfo;
+
+    const fullName = name.trim() !== '';
+    const fullLogin = login.trim() !== '';
+    const fullPassword = senha !== '';
+    const passwordMin = senha.length >= 8;
+    const passwordMax = senha.length <= 16;
+    const passwordHaveLettersAndNumbers = /[a-zA-Z]/.test(senha) && /\d/.test(senha);
+    const passwordCaracterEspecial = /[!@#$%^&*]/.test(senha);
+
+    const formValidYOrN = (fullName
+       && fullLogin
+       && fullPassword
+       && passwordMin
+       && passwordMax
+       && passwordHaveLettersAndNumbers
+       && passwordCaracterEspecial);
+    setRegisterButton(!formValidYOrN);
+  }
+
   return (
     <form>
-      <label htmlFor="nome">
+      <label htmlFor="name">
         Nome do serviço
-        <input id="nome" type="text" />
+        <input
+          id="name"
+          type="text"
+          name="name"
+          value={ formInfo.name }
+          onChange={ handleChange }
+          required
+        />
       </label>
-      <label htmlFor="Login">
+      <label htmlFor="login">
         Login
-        <input id="Login" type="text" />
+        <input
+          id="login"
+          type="text"
+          name="login"
+          value={ formInfo.login }
+          onChange={ handleChange }
+          required
+        />
       </label>
-      <label htmlFor="Senha">
+      <label htmlFor="senha">
         Senha
-        <input id="Senha" type="password" />
+        <input
+          id="senha"
+          type="password"
+          name="senha"
+          value={ formInfo.senha }
+          onChange={ handleChange }
+          required
+        />
       </label>
-      <label htmlFor="URL">
+      <label htmlFor="url">
         URL
-        <input id="URL" type="text" />
+        <input
+          id="url"
+          type="text"
+          name="url"
+          value={ formInfo.url }
+          onChange={ handleChange }
+        />
       </label>
-      <button>Cadastrar</button>
+      <button disabled={ registerButton }>Cadastrar</button>
       <button onClick={ () => handleForm(false) }>Cancelar</button>
     </form>
   );
